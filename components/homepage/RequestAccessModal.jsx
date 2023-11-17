daa762ea286544022f06146ecfde6c0e21ad5f1c
Matt Gabriel Domingo <mattgabriel4299@gmail.com>
2023-11-18 02:43:00 +0800

    req access modal

--- components/homepage/RequestAccessModal.jsx
+++ components/homepage/RequestAccessModal.jsx
@@ -96,44 +96,9 @@ const RequestAccessModal = ({isModalVisible, setModalVisible}) => {
       animationOut="fadeOut">
         <View style={styles.modalWrapper}>
             <View style={[styles.modalHeader]}>
-              <Text style={[styles.textBold, styles.textHeader]}>User List</Text>
+              <Text style={[styles.textBold, styles.textHeader]}>Request Access</Text>
               <AntDesign name="close" size={24} color="#3A85AF" onPress={toggleModal}/>
-            </View>
-
-            {isEdit ? 
-            <View>
-              <ScrollView
-                  contentContainerStyle={{
-                    gap: 8,
-                  }}
-                  style={{
-                    marginVertical: 8,
-                    maxHeight: '80%',
-                  }}>  
-                {userList && userList.map((item, idx) =>
-                <View key={idx} style={{alignItems: 'center', flexDirection: 'row'}}>
-                  <View style={{alignItems: 'center', flexDirection: 'row', gap: 8, flex: 1}}>
-                    <Image source={`https:${item.Images.split(":")[1]}`} style={{width: 40, height: 40, borderRadius: 50, justifyContent: 'center'}}/>
-                    <Text>{item.userName}</Text>
-                  </View>
-                  {(item.userName !== activeUser && item.Email !== activeBudget.budgetOwner) && 
-                  <Pressable onPress={() => removeUserHandler(item.userName, item.Email)}>
-                    <View style={{width: 30, height:30, backgroundColor: COLORS['red-500'], borderRadius:8, alignItems: 'center', justifyContent: 'center'}}>
-                      <AntDesign name="deleteuser" size={24} color={COLORS['white-500']} />
-                    </View>
-                  </Pressable>
-                  }
-                </View>)}
-              </ScrollView>
-              <View style={{
-                    borderTopColor: 'black',
-                    borderTopWidth: 4,
-                    }}>
-                <TextInput placeholder='Enter user email address...' value={userEmail} onChangeText={setUserEmail}/>
-                <Button label={"Add User"} action={() => addUserHandler()} isLoading={isLoading} />
-              </View>
-            </View>
-            :
+            </View>       
             <View>
               <View style={{
                     flexDirection: 'row',
@@ -147,11 +112,14 @@ const RequestAccessModal = ({isModalVisible, setModalVisible}) => {
                 <View key={idx} style={{alignItems: 'center'}}>
                   <Image source={`https:${user.Images.split(":")[1]}`} style={{width: 65, height: 65, borderRadius: 50, justifyContent: 'center'}}/>
                   <Text>{user.userName}</Text>
+
+                  <View>
+                    
+                  </View>
                 </View>)}
               </View>
               <Button label={"Edit Userlist"} action={() => setIsEdit(!isEdit)} />
             </View>
-            }
         </View>
     </Modal>
   )
